3c0cb743c5e88c01325f74b4c0ef320099a2bb6c
nabiladigo <nabiladigo91@gmail.com>
2022-01-16 01:33:53 -0500

    update files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const  app = express();
+const productsController = require('./controllers/products-controller.js');
+const PORT = 2000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('puplic'));
+app.use('/products', productsController);
+app.use((req, res, next)=>{
+    next();
+})
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!')
+})
+app.get('/*', (req, res)=>{
+    const context = {error: req.error};
+    return res.status(404).render('404', context);
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`I am listening on port${PORT}`);
+});
